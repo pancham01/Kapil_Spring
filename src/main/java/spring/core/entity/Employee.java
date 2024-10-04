db4f336c29eec722af58f142c7f3847421f87166
pancham01 <pancham9557@gmail.com>
2024-10-04 09:55:43 +0530

    update

--- src/main/java/spring/core/entity/Employee.java
+++ src/main/java/spring/core/entity/Employee.java
@@ -1,12 +1,19 @@
 package spring.core.entity;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 
 	private int id;
 	private String name;
 	private String gender;
-
 	private Address address;
+
+	private List<Integer> contacts;
+	private Set<Integer> set;
+	private Map<Integer, String> map;
 
 	public Employee() {
 		super();
@@ -14,12 +21,40 @@ public class Employee {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Employee(int id, String name, String gender, Address address) {
+	public Employee(int id, String name, String gender, Address address, List<Integer> contacts, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
+		this.contacts = contacts;
+		this.set = set;
+		this.map = map;
+	}
+
+	public List<Integer> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Integer> contacts) {
+		this.contacts = contacts;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
 	}
 
 	public Address getAddress() {
@@ -59,7 +94,8 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + ", contacts="
+				+ contacts + ", set=" + set + ", map=" + map + "]";
 	}
 
 	
